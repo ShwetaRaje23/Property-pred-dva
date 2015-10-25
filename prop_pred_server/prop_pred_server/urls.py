1f4f7c6d8345e09f1e50f8f8d4d6ba0854e8521a
Shweta <Shweta@lawn-128-61-55-206.lawn.gatech.edu>
2015-10-25 19:29:13 -0400

    added basic login page

--- prop_pred_server/prop_pred_server/urls.py
+++ prop_pred_server/prop_pred_server/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'prop_pred_server.views.index')
+    url(r'^$', 'prop_pred_server.views.login')
 ]
--- prop_pred_server/prop_pred_server/views.py
+++ prop_pred_server/prop_pred_server/views.py
@@ -6,3 +6,8 @@ def index(request):
 	template = loader.get_template('index.html')
 	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
+
+def login(request):
+	template = loader.get_template('login.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
